f5ae55ed4b8c6d81d1ddcd10c64f4879db7d7657
ZiadAhmed92 <za693387@gmail.com>
2023-12-29 17:36:57 +0200

    edit cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000
 
 
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://crud-mongoose-ten.vercel.app/' }));
 app.use(router)
 
 
